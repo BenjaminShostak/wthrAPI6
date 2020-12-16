de991a048b4df92c0393df89137dac203cb913bb
Benjamin Shostak <benshostak@rocketmail.com>
2020-12-16 02:05:57 -0800

    added more js

---
+++ index.js
@@ -0,0 +1,13 @@
+const form = document.querySelector(".top-banner form");
+ 
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  const inputVal = input.value;
+});
+
+const apiKey = "YOUR_OWN_KEY";
+const inputVal = input.value;
+ 
+...
+ 
+const url =
